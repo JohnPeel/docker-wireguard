8c0739425af110774cea12c2aca41c1545fec33c
John Peel <john@dgby.org>
2021-07-22 16:13:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
 set -e
-
-if lsmod | grep wireguard &> /dev/null ; then
-  echo "wireguard kernel module is already loaded"
-else
-  echo "Building wireguard kernel module..."
-  make module
-  
-  echo "Installing wireguard kernel module..."
-  make module-install
-  
-  echo "Module installed."
-fi
-
 exec $@
